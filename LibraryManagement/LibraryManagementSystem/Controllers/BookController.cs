0ed41deaaed07277349463b24d3db8599991d9aa
Tamek <jameanderson2004@gmail.com>
2024-11-22 08:30:02 +0700

    Library add detail, index action in BookController

--- LibraryManagement/LibraryManagementSystem/Controllers/BookController.cs
+++ LibraryManagement/LibraryManagementSystem/Controllers/BookController.cs
@@ -1,11 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using LibraryManagementSystem.Models;
 
 namespace LibraryManagementSystem.Controllers
 {
     public class BookController : Controller
     {
-        public IActionResult Index()
+        private readonly LibraryDbContext _context;
+
+        public BookController(LibraryDbContext context)
         {
+            _context = context;
+        }
+        
+        // GET list book
+        public async Task<IActionResult> Index()
+        {
+            return _context.Books != null ?
+                View(await _context.Books.ToListAsync()) :
+                Poblem("Book not found.");
+        }
+
+        public async Task<IActionResult> Details(int? Id)
+        {
+            if (Id == null || _context.Books == null)
+            {
+                return Notfound();
+            }
+            var book = await _context.Books
+                .FirstOrDefaultAsync(b=>b.Id == Id);
             return View();
         }
     }
