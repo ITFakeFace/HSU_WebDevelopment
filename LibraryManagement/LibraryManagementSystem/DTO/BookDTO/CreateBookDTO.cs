aba6e1bfc9ed1dcc6c21ec8a8a72929eac71ecec
Khôi Nguyễn <khoinm0603@gmail.com>
2024-12-14 14:39:47 +0700

    Add CreateBookDTO

---
+++ LibraryManagement/LibraryManagementSystem/DTO/BookDTO/CreateBookDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace LibraryManagementSystem.DTO.BookDTO
+{
+    public class CreateBookDTO
+    {
+        public required string Title { get; set; }
+        public string? Description { get; set; }
+        public required int AuthorId { get; set; }
+        public int? VendorId { get; set; }
+        public int? PublishYear { get; set; }
+        public int? PageNumber { get; set; }
+        public required string Language { get; set; }
+        public required string Version { get; set; }
+        public int? SeriesId { get; set; }
+        public required string ISBN { get; set; }
+    }
+}
