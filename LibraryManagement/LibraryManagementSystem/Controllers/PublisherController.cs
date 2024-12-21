9beabbc06069ce221cc6431241dc48a48fc1d869
phước em <phuocanh554@gmail.com>
2024-12-21 16:36:35 +0700

    update indext and search and publis

---
+++ LibraryManagement/LibraryManagementSystem/Controllers/PublisherController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagementSystem.Controllers
+{
+    public class PublisherController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
